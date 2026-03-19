74bbfe765a876a4d9eb70ba62a72d1716c1d36f8
Dmitry Gotskin <dmitry@gotskin.ru>
2026-03-19 08:40:56 +0300

    feat(ui): add settings page with routing

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import SettingsPage from './pages/settings/settings-page.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
